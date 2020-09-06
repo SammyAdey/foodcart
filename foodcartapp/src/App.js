aac238ee4e7575c2b9a5ace18265cfde7d7665ea
SammyAdey <samuel3adeyemi@live.com>
2020-09-07 00:44:56 +0100

    Checkout section added

--- foodcartapp/src/App.js
+++ foodcartapp/src/App.js
@@ -7,28 +7,28 @@ class App extends Component {
     items: [
       {
         id: 3455890,
-        value: 0,
+        value: 1,
         title: "Coconut Butter Milk",
         price: 8.5,
         img: "https://picsum.photos/100",
       },
       {
         id: 7456890,
-        value: 0,
+        value: 1,
         title: "Chocolate Donuts",
         price: 4.5,
         img: "https://picsum.photos/100",
       },
       {
         id: 545890,
-        value: 0,
+        value: 1,
         title: "Dried Fish",
         price: 11.9,
         img: "https://picsum.photos/100",
       },
       {
         id: 3477891,
-        value: 0,
+        value: 1,
         title: "Powedered donuts",
         price: 10.5,
         img: "https://picsum.photos/100",
@@ -48,7 +48,9 @@ class App extends Component {
     const items = [...this.state.items];
     const index = items.indexOf(item);
     items[index] = { ...item };
-    items[index].value--;
+    if (items[index].value > 0) {
+      items[index].value--;
+    }
     this.setState({ items });
   };
 
@@ -66,19 +68,29 @@ class App extends Component {
     this.setState({ items });
   };
 
-  // totalPrice = () => {
-  //   const totalprice = this.state.items.map((c) => {
-  //     total += c.price;
-  //     return c;
-  //   });
-  //   return totalprice;
-  // };
+  totalPrice = (items) => {
+    var itemsPrice = 0;
+    var itemPrice = 0;
+    let totalprice = items.forEach((item) => {
+      itemPrice = item.price * item.value;
+      itemsPrice += itemPrice;
+      return itemsPrice;
+    });
+    console.log(itemsPrice);
+    return itemsPrice;
+  };
+
+  cartTitle() {
+    const value = this.state.items.length;
+    return value > 0 ? "Shopping Cart" : "Cart is Empty";
+  }
 
   render() {
     return (
       <React.Fragment>
         <main className="cart-container">
           <div className="cart">
+            <h3 className="cart-title">{this.cartTitle()}</h3>
             <Items
               items={this.state.items}
               onDelete={this.handleDelete}
@@ -86,7 +98,77 @@ class App extends Component {
               onDecrement={this.handleDecrement}
             />
             <div className="priceTotal">
-              Total: {this.state.items.totalPrice}
+              Total: <h1>${this.totalPrice(this.state.items)}</h1>
+            </div>
+            <div className="credit-section">
+              {/* <h3 className="cart-title">card Details</h3> */}
+              <div className="col-md-6">
+                <div className="credit-card">
+                  <h3>Lorem Ipsum</h3>
+                  <h5>**** **** **** ****</h5>
+                  <div className="card-date">12/25</div>
+                  <div className="card-bottom">
+                    <div className="card-name">Dana Smith</div>
+                    <div className="card-logo"></div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-6">
+                <div className="credit-form">
+                  <form>
+                    <div className="form-row">
+                      <div className="form-group col-md-12">
+                        <label for="inputEmail4">Name On Card</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="inputEmail4"
+                          placeholder="Email"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="form-group col-md-12">
+                        <label for="inputEmail4">Card Number</label>
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="inputEmail4"
+                          placeholder="Email"
+                        />
+                      </div>
+                    </div>
+                    <label for="inputState">Experation Date</label>
+                    <label for="inputEmail4">CVV</label>
+                    <div className="form-row">
+                      <div className="form-group col-md-4">
+                        <select id="inputState" className="form-control">
+                          <option selected>mm</option>
+                          <option>...</option>
+                        </select>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <select id="inputState" className="form-control">
+                          <option selected>yyyy</option>
+                          <option>...</option>
+                        </select>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <input
+                          type="number"
+                          className="form-control"
+                          placeholder="xxx"
+                          id="inputEmail4"
+                        />
+                      </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                      Check Out
+                    </button>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
         </main>
--- foodcartapp/src/components/item.jsx
+++ foodcartapp/src/components/item.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-// import { Add } from "material-ui-icons";
+import { MdAdd } from "react-icons/md";
+import { MdRemove } from "react-icons/md";
+import { MdClear } from "react-icons/md";
 
 class Item extends Component {
   render() {
     return (
       <div className="Item">
-        <div className="item-details">
+        <div className="item-details col-7">
           <div className="item-thumbnail mr-3">
             <img src={this.props.item.img} alt="" />
           </div>
@@ -14,26 +16,37 @@ class Item extends Component {
             <small>#{this.props.item.id}</small>
           </div>
         </div>
-        <div className="item-actions">
-          <div className="itemCount mr-3">
-            <div onClick={() => this.props.onIncrement(this.props.item)}>
-              <Add />
+
+        <div className="item-actions col-5">
+          <div className="itemCount mr-3 col-5">
+            <div
+              className="action-icon"
+              onClick={() => this.props.onIncrement(this.props.item)}
+            >
+              <MdAdd />
             </div>
+
             <div className="nOfItems">{this.props.item.value}</div>
-            <div onClick={() => this.props.onDecrement(this.props.item)}>-</div>
+
+            <div
+              className="action-icon"
+              onClick={() => this.props.onDecrement(this.props.item)}
+            >
+              <MdRemove className="" />
+            </div>
           </div>
 
-          <span className="price mr-3">
+          <div className="price col-5">
             <span className="currency">$</span>
             {this.props.item.price}0
-          </span>
+          </div>
 
-          <span
-            className="item-action"
+          <div
+            className="item-action col-2"
             onClick={() => this.props.onDelete(this.props.item.id)}
           >
-            x
-          </span>
+            <MdClear />
+          </div>
         </div>
       </div>
     );
